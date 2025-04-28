891e492641b7413ba690a40a475e133435ad7377
Filip Krása <102549834+Fidasek009@users.noreply.github.com>
2025-04-28 23:11:40 +0200

    Add project files.

---
+++ WCecko/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Mapsui.Tiling;
+using Mapsui.UI.Maui;
+using Mapsui.Widgets;
+
+namespace WCecko
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var mapControl = new MapControl();
+            var map = mapControl.Map;
+
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+
+            Content = mapControl;
+        }
+    }
+}
